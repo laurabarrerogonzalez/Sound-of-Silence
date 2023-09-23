24e29228bc3d3aee0e85b296c35a39e05c180adc
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-09-23 18:03:30 +0200

    Login-inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './views/Home/Home.jsx'
-// import Login from './views/Login/Login.jsx'
+
+import Login from './views/Login/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Login /> */}
-    <Home />
+    { <Login /> }
+  
   </React.StrictMode>,
 )
