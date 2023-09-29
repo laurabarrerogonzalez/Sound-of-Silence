06281bb2be3573f23bab4ca9ecf198ced7a1969c
Anthony Brayan <brayan.farronan@gmail.com>
2023-09-29 16:29:46 +0200

    All login ok

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,32 +1,145 @@
-import React, { useState } from 'react';
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
   const [isLoginFormVisible, setIsLoginFormVisible] = useState(true);
   const [isLoginFormSubmitted, setIsLoginFormSubmitted] = useState(false);
+
   const loginDataInitialState = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   };
+
   const [loginData, setLoginData] = useState(loginDataInitialState);
-  const createAccountDataInitialState = {
-    Name_user: '',
-    email: '',
-    password: '',
+
+  const errorMessagesLoginDataInitialState = {
+    email: "",
+    password: "",
   };
-  const [createAccountData, setCreateAccountData] = useState(createAccountDataInitialState);
+  const [errorMessagesLogin, setErrorMessagesLogin] = useState(
+    errorMessagesLoginDataInitialState
+  );
+
+  const createAccountDataInitialState = {
+    Name_user: "",
+    email: "",
+    password: "",
+  };
+
+  const [createAccountData, setCreateAccountData] = useState(
+    createAccountDataInitialState
+  );
+
   const errorMessagesInitialState = {
-    Name_user: '',
-    email: '',
-    password: '',
+    Name_user: "",
+    email: "",
+    password: "",
   };
   const [errorMessages, setErrorMessages] = useState(errorMessagesInitialState);
+
   const navigate = useNavigate();
+
   const toggleLoginForm = () => {
     setIsLoginFormVisible(!isLoginFormVisible);
   };
+  const handleLoginSubmit = async () => {
+    const { email, password } = loginData;
+  
+    const newErrorMessages = {
+      email: !email ? "Email is required" : "",
+      password: !password ? "Password is required" : "",
+    };
+  
+    if (!email || !password) {
+      setErrorMessagesLogin(newErrorMessages);
+      return;
+    }
+  
+    try {
+      const url = "https://localhost:7134/Users/Login";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+  
+      if (response.ok) {
+        const responseData = await response.json();
+        const userRole = responseData.role;
+        if (userRole === 1) {
+          navigate("/admin");
+        } else if (userRole === 2) {
+          navigate("/subscribe");
+        } else {
+          Swal.fire("Error", "Usuario no autorizado", "error");
+        }
+      } else {
+        setErrorMessagesLogin("Credenciales incorrectas");
+        Swal.fire("Error", "Usuario no existente", "error");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      Swal.fire("Error", "Ha ocurrido un error en el servidor", "error");
+    } finally {
+      // Limpia los campos después del inicio de sesión y restablece los mensajes de error
+      setLoginData({
+        email: "",
+        password: "",
+      });
+      setErrorMessagesLogin({
+        email: "",
+        password: "",
+      });
+  
+      // Establece isLoginFormSubmitted en false para habilitar el botón de inicio de sesión
+      setIsLoginFormSubmitted(false);
+    }
+  };
+
+  const handleCreateAccountSubmit = async () => {
+    const { Name_user, email, password } = createAccountData;
+    const newErrorMessages = {
+      Name_user: !Name_user ? "Name is required" : "",
+      email: !email ? "Email is required" : "",
+      password: !password ? "Password is required" : "",
+    };
+    if (!Name_user || !email || !password) {
+      setErrorMessages(newErrorMessages);
+      return;
+    }
+    try {
+      const url = "https://localhost:7134/Users/Post";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ Name_user, email, password }),
+      });
+      if (response.ok) {
+        Swal.fire("Cuenta creada exitosamente", "", "success");
+      } else {
+        Swal.fire("Error", "No se pudo crear la cuenta", "error");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      Swal.fire("Error", "Ha ocurrido un error en el servidor", "error");
+    }
+    // Limpia los campos después del inicio de sesión y restablece los mensajes de error
+    setCreateAccountData({
+      email: "",
+      password: "",
+    });
+    setErrorMessages({
+      email: "",
+      password: "",
+    });
+  };
+
   const handleLoginInputChange = (e) => {
     const { name, value } = e.target;
     setLoginData({
@@ -34,6 +147,7 @@ const Login = () => {
       [name]: value,
     });
   };
+
   const handleCreateAccountInputChange = (e) => {
     const { name, value } = e.target;
     setCreateAccountData({
@@ -41,81 +155,21 @@ const Login = () => {
       [name]: value,
     });
   };
-  const handleLoginSubmit = async () => {
-    setIsLoginFormSubmitted(true);
-    const { email, password } = loginData;
-    const newErrorMessages = {
-      email: !email ? 'Email is required' : '',
-      password: !password ? 'Password is required' : '',
-    };
-    if (!email || !password) {
-      setErrorMessages(newErrorMessages);
-      return;
-    }
-    try {
-      const url = 'https://localhost:7134/Users/Login';
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      if (response.ok) {
-        const responseData = await response.json();
-        const userRole = responseData.role;
-        if (userRole === 1) {
-          navigate('/admin');
-        } else if (userRole === 2) {
-          navigate('/subscribe');
-        } else {
-          Swal.fire('Error', 'Usuario no autorizado', 'error');
-        }
-      } else {
-        Swal.fire('Error', 'Credenciales incorrectas', 'error');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      Swal.fire('Error', 'Ha ocurrido un error en el servidor', 'error');
-    }
-  };
-  const handleCreateAccountSubmit = async () => {
-    const { Name_user, email, password } = createAccountData;
-    const newErrorMessages = {
-      Name_user: !Name_user ? 'Name is required' : '',
-      email: !email ? 'Email is required' : '',
-      password: !password ? 'Password is required' : '',
-    };
-    if (!Name_user || !email || !password) {
-      setErrorMessages(newErrorMessages);
-      return;
-    }
-    try {
-      const url = 'https://localhost:7134/Users/Post';
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ Name_user, email, password }),
-      });
-      if (response.ok) {
-        Swal.fire('Cuenta creada exitosamente', '', 'success');
-      } else {
-        Swal.fire('Error', 'No se pudo crear la cuenta', 'error');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      Swal.fire('Error', 'Ha ocurrido un error en el servidor', 'error');
-    }
-  };
+
   return (
     <div className="container-form">
       <video autoPlay loop muted className="video-background">
-        <source src="https://res.cloudinary.com/dit2zhtwz/video/upload/v1695463586/The_sea_bugriz.mp4" type="video/mp4" />
+        <source
+          src="https://res.cloudinary.com/dit2zhtwz/video/upload/v1695463586/The_sea_bugriz.mp4"
+          type="video/mp4"
+        />
       </video>
       <div className="logo-container">
-        <img src="https://res.cloudinary.com/dqc0wvttr/image/upload//e_improve,e_sharpen/v1695634508/Captura_de_pantalla_2023-09-23_215716_nqz4vb-removebg-preview_lwpq0u.png" alt="Logo" className="logo" />
+        <img
+          src="https://res.cloudinary.com/dqc0wvttr/image/upload//e_improve,e_sharpen/v1695634508/Captura_de_pantalla_2023-09-23_215716_nqz4vb-removebg-preview_lwpq0u.png"
+          alt="Logo"
+          className="logo"
+        />
       </div>
       <div className="welcome-back">
         {isLoginFormVisible ? (
@@ -129,7 +183,7 @@ const Login = () => {
                 value={loginData.email}
                 onChange={handleLoginInputChange}
               />
-              <span className="error-message">{errorMessages.email}</span>
+              <span className="error-message">{errorMessagesLogin.email}</span>
               <input
                 type="password"
                 placeholder="Password"
@@ -137,7 +191,9 @@ const Login = () => {
                 value={loginData.password}
                 onChange={handleLoginInputChange}
               />
-              <span className="error-message">{errorMessages.password}</span>
+              <span className="error-message">
+                {errorMessagesLogin.password}
+              </span>
               {isLoginFormSubmitted ? (
                 <input type="button" value="Login" className="custom-color" />
               ) : (
@@ -149,19 +205,26 @@ const Login = () => {
                 />
               )}
             </form>
-            <div className={`message ${isLoginFormVisible ? '' : 'hide'}`}>
+            <div className={`message ${isLoginFormVisible ? "" : "hide"}`}>
               <div className="welcome-text">
                 <h2>Welcome to Sound of Silence</h2>
                 <p>If you already have an account please login here</p>
               </div>
-              <button className="custom-signup-button" onClick={toggleLoginForm}>
+              <button
+                className="custom-signup-button"
+                onClick={toggleLoginForm}
+              >
                 Sign up
               </button>
             </div>
           </>
         ) : (
           <>
-            <div className={`message white-text bold-text ${isLoginFormVisible ? 'hide' : ''}`}>
+            <div
+              className={`message white-text bold-text ${
+                isLoginFormVisible ? "hide" : ""
+              }`}
+            >
               <div className="welcome-text">
                 <h2>Welcome to Sound of Silence</h2>
                 <p>If you don't have an account please register here</p>
@@ -198,7 +261,11 @@ const Login = () => {
               />
               <span className="error-message">{errorMessages.password}</span>
               {isLoginFormSubmitted ? (
-                <input type="button" value="Sign up" className="custom-signup-button" />
+                <input
+                  type="button"
+                  value="Sign up"
+                  className="custom-signup-button"
+                />
               ) : (
                 <input
                   type="button"
@@ -215,5 +282,3 @@ const Login = () => {
   );
 };
 export default Login;
-
-
