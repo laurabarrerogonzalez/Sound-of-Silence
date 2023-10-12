6badd485c9797732a7081abaa381189b1eb710ad
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-10-12 13:04:25 +0200

    Subscribe

--- src/views/Admin/Admin.jsx
+++ src/views/Admin/Admin.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const Admin = () => {
   const navigate = useNavigate();
 
+  
   const [formData, setFormData] = useState({
     videoSrc: '',
     audioSrc: '',
